cd11f3ceb802dfc642b8655b2c3633b8670a5c53
kaichen <3069394597@qq.com>
2024-03-04 16:42:24 +0800

    项目实例上传

---
+++ src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Logo />
+  </div>
+</template>
+<script>
+
+    </script>
+    <style>
+    .login-page {
+      background-image: url("../assets/img/logo.png");
+      background-size: cover;
+    }
+
+    </style>
